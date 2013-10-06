ec102b37e6a147d3d5f75b9e00653336dd67774a
danbraik <danbraik@hotmail.com>
2013-10-06 15:07:05 +0200

    Add Actor

---
+++ js/core/actor.js
@@ -0,0 +1,17 @@
+// Actor
+// Moveable object
+
+var Actor = function () {
+	this.pos = new Vector(0,0);
+}
+
+Actor.prototype = {
+	getPosition: function () {
+		return this.pos;
+	},
+
+	setPosition: function (x,y) {
+		this.pos.x = x;
+		this.pos.y = y;
+	}
+}
