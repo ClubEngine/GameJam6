84c58b558908a77b8e9d4623e1663a3127bed594
Benoit Person <benoit.person@gmail.com>
2013-10-07 00:26:10 +0200

    js/main.js: some cleaning + todos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
 	SoundManager.init();
 
-	LabyrinthFactory.parseFromFile('maps/map1.txt', function (lab) {
-		console.log(lab);
+	// @TODO-FIX: why do we hear the walking sound when
+	// we load the page ? It stops after the first move
+	// though
 
+	// @TODO: this entire loop should be reduced a lot,
+	// splitting each specific block and putting them
+	// where they need to be would be a good start
+	LabyrinthFactory.parseFromFile('maps/map1.txt', function (lab) {
 		var push_date = Date.now();
 		var ball_date = Date.now();
 		var anim_date = Date.now();
@@ -34,7 +39,6 @@ $(document).ready(function () {
 		}
 		// end spawn monsters
 
-
 		var graphics = new Graphics(startGame);
 
 		function startGame () {
@@ -44,190 +48,167 @@ $(document).ready(function () {
 			kd.run(function () {
 				kd.tick();
 
-			// prevent player to move super quickly	
-			if (action.state != Action.IDLE) {	
-				if (Date.now() > push_date + 150) {
-					push_date = Date.now();
+				// prevent player to move super quickly	
+				if (action.state != Action.IDLE) {	
+					if (Date.now() > push_date + 150) {
+						push_date = Date.now();
 					
-					if (!doMovement(player, lab, action.state)) {
-						playPas();
-						window.scrollTo((player.getPosition().x-5)*32, (player.getPosition().y-5)*32);
-					}
+						if (!doMovement(player, lab, action.state)) {
+							playPas();
+							window.scrollTo((player.getPosition().x-5)*32, (player.getPosition().y-5)*32);
+						}
 		
-					if (action.state >= Action.FIRE_U && action.state <= Action.FIRE_L) {
-						ball = new Actor();
-						ball.setPosition(player.getPosition().x, player.getPosition().y);
-						ball.setSpriteId(SpriteCode.FIRE_BALL);
-						ball.setDirection(action.state);
-						entities.push(ball);
-						playBall();
+						if (action.state >= Action.FIRE_U && action.state <= Action.FIRE_L) {
+							ball = new Actor();
+							ball.setPosition(player.getPosition().x, player.getPosition().y);
+							ball.setSpriteId(SpriteCode.FIRE_BALL);
+							ball.setDirection(action.state);
+							entities.push(ball);
+							playBall();
+						}
 					}
 				}
-			}
 
-			// Move fire balls
-			if (Date.now() > ball_date + 100) {
-							ball_date = Date.now();
+				// Move fire balls
+				if (Date.now() > ball_date + 100) {
+					ball_date = Date.now();
+						
+					newEntities = entities.slice();
+					for (var i in entities) {
+						if (entities[i].getSpriteId() == SpriteCode.FIRE_BALL){
+							var ball = entities[i];
+							toRm = doMovementFireBall(ball, lab, ball.getDirection());
 							
-							newEntities = entities.slice();
-							for (var i in entities) {
-									if(entities[i].getSpriteId() == SpriteCode.FIRE_BALL){
-											var ball = entities[i];
-											toRm = doMovementFireBall(ball, lab, ball.getDirection());
-											if (!toRm) {
-													for (var j in entities) {
-															if (i!=j) {
-																	tmp_other = entities[j];
-																	if (ball.getPosition().x == tmp_other.getPosition().x &&
-																			ball.getPosition().y == tmp_other.getPosition().y) {
-																			newEntities = delTabElement(newEntities, tmp_other);    
-																			toRm = true;
-																	}
-															}
-													}       
-											}       
-											if (toRm) {
-													newEntities = delTabElement(newEntities, ball);
-							playDepop();
-											}
-
-					} 
+							if (!toRm) {
+								for (var j in entities) {
+									if (i!=j) {
+										tmp_other = entities[j];
+										if (ball.getPosition().x == tmp_other.getPosition().x &&
+											ball.getPosition().y == tmp_other.getPosition().y) {
+											newEntities = delTabElement(newEntities, tmp_other);    
+											toRm = true;
+										}
+									}
+								}       
 							}
-							entities = newEntities;
-					}
 
-			// Anim monsters. Move to a neighbour case randomly.
-			if (Date.now() > anim_date + 1000) {		
-				anim_date = Date.now();
-				for (var i in entities) {
-					if (entities[i].getSpriteId() == SpriteCode.MONSTER1) {
-						var rdir = Math.floor(Math.random()*4);
-					console.log(rdir);
-						var vdir = new Vector(0, 0);
-						switch(rdir) {
-							case 0:
-								vdir.x = 1;
-								break;
-							case 1:
-								vdir.x = -1;
-								break;
-							case 2:
-								vdir.y = 1;
-								break;
-							case 3:
-								vdir.y = -1;
-								break;
-						}
-						var cpos = entities[i].getPosition();
-			
-						if(!lab.isObstacle(cpos.x+vdir.x, cpos.y+vdir.y)) {
-							entities[i].setPosition(cpos.x+vdir.x, cpos.y+vdir.y);	
-						}
+							if (toRm) {
+								newEntities = delTabElement(newEntities, ball);
+								playDepop();
+							}
+						} 
 					}
+					
+					entities = newEntities;
 				}
-			}		
 
-			graphics.refreshAll(entities);
+				// Anim monsters. Move to a neighbour case randomly.
+				if (Date.now() > anim_date + 1000) {		
+					anim_date = Date.now();
+					for (var i in entities) {
+						if (entities[i].getSpriteId() == SpriteCode.MONSTER1) {
+							var rdir = Math.floor(Math.random()*4);
+							var vdir = new Vector(0, 0);
+							switch(rdir) {
+								case 0:
+									vdir.x = 1;
+									break;
+								case 1:
+									vdir.x = -1;
+									break;
+								case 2:
+									vdir.y = 1;
+									break;
+								case 3:
+									vdir.y = -1;
+									break;
+							}
+							var cpos = entities[i].getPosition();
+				
+							if(!lab.isObstacle(cpos.x+vdir.x, cpos.y+vdir.y)) {
+								entities[i].setPosition(cpos.x+vdir.x, cpos.y+vdir.y);	
+							}
+						}
+					}
+				}		
+
+				graphics.refreshAll(entities);
 			});	
 		}
-	});	
-
-
+	});
 });
 
 
-function playPas()
-{
+//= Sound management
+// @TODO: should be moved to the SoundManager
+function playPas () {
 	SoundManager.play("Pas");
 }
-function playBall()
-{
+
+function playBall () {
 	SoundManager.play("Ball");
 }
-function playDepop()
-{
+
+function playDepop () {
 	SoundManager.play("Depop");
 }
-function playNorth()
-{
-	i=Math.floor((Math.random()*100)%7)+1;
-	SoundManager.play('North'+i);
 
-
-}
-function playSouth()
-{
-	i=Math.floor((Math.random()*100)%7)+1;
-	SoundManager.play('South'+i);
-
+function playNorth () {
+	i = Math.floor((Math.random() * 100) % 7) + 1;
+	SoundManager.play('North' + i);
 }
 
-function playEast()
-{
-	i=Math.floor((Math.random()*100)%4)+1;
-	SoundManager.play('East'+i);
-
+function playSouth () {
+	i = Math.floor((Math.random() * 100) % 7) + 1;
+	SoundManager.play('South' + i);
 }
 
-
-function playWest()
-{
-	i=Math.floor((Math.random()*100)%3)+1;
-	SoundManager.play('West'+i);
-
+function playEast () {
+	i = Math.floor((Math.random() * 100) % 4) + 1;
+	SoundManager.play('East' + i);
 }
 
-
-function playYes()
-{
-	i=Math.floor((Math.random()*100)%5)+1;
-	SoundManager.play('Yes'+i);
+function playWest () {
+	i = Math.floor((Math.random() * 100) % 3) + 1;
+	SoundManager.play('West' + i);
 }
 
-function playNo()
-{
-	i=Math.floor((Math.random()*100)%6)+1;
-	SoundManager.play('No'+i);
-}
-function playRNorth()
-{
-	i=Math.floor((Math.random()*100)%7)+1;
-	SoundManager.play('RNorth'+i);
-
-
-}
-function playRSouth()
-{
-	i=Math.floor((Math.random()*100)%7)+1;
-	SoundManager.play('RSouth'+i);
-
+function playYes () {
+	i = Math.floor((Math.random() * 100) % 5) + 1;
+	SoundManager.play('Yes' + i);
 }
 
-function playREast()
-{
-	i=Math.floor((Math.random()*100)%4)+1;
-	SoundManager.play('REast'+i);
-
+function playNo () {
+	i = Math.floor((Math.random() * 100) % 6) + 1;
+	SoundManager.play('No' + i);
 }
 
-
-function playRWest()
-{
-	i=Math.floor((Math.random()*100)%3)+1;
-	SoundManager.play('RWest'+i);
-
+function playRNorth () {
+	i = Math.floor((Math.random() * 100) % 7) + 1;
+	SoundManager.play('RNorth' + i);
 }
 
-
-function playRYes()
-{
-	i=Math.floor((Math.random()*100)%5)+1;
-	SoundManager.play('RYes'+i);
+function playRSouth () {
+	i = Math.floor((Math.random() * 100) % 7) + 1;
+	SoundManager.play('RSouth' + i);
 }
 
-function playRNo()
-{
-	i=Math.floor((Math.random()*100)%6)+1;
-	SoundManager.play('RNo'+i);
+function playREast () {
+	i = Math.floor((Math.random() * 100) % 4) + 1;
+	SoundManager.play('REast' + i);
 }
 
+function playRWest () {
+	i = Math.floor((Math.random() * 100) % 3) + 1;
+	SoundManager.play('RWest' + i);
+}
+
+function playRYes () {
+	i = Math.floor((Math.random() * 100) % 5) + 1;
+	SoundManager.play('RYes' + i);
+}
+
+function playRNo () {
+	i = Math.floor((Math.random() * 100) % 6) + 1;
+	SoundManager.play('RNo' + i);
+}
